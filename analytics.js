0eb6e36e18606cd48e8143d11cc2d8a82eed867b
anyconversion <anyconversion@gmail.com>
2025-04-27 20:35:05 -0400

    Delete analytics.js

--- analytics.js
+++
@@ -1,5 +0,0 @@
-// analytics.js
-window.dataLayer = window.dataLayer || [];
-function gtag() { dataLayer.push(arguments); }
-gtag('js', new Date());
-gtag('config', 'G-25CMMZLTZ7', { 'page_path': window.location.pathname });
